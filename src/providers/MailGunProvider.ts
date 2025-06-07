7200ff779b88c9745bcf6a627ecf235bdbc6eebe
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-06-07 17:51:32 +0530

    commits for cloud

--- src/providers/MailGunProvider.ts
+++ src/providers/MailGunProvider.ts
@@ -2,7 +2,7 @@ import { EmailRequest } from "../models/EmailRequest";
 
 export class MailgunProvider {
   async sendEmail(email: EmailRequest): Promise<void> {
-    const success = Math.random() > 0.9; // Sample 20% success chance.
+    const success = Math.random() > 0.9; // Sample 10% success chance.
 
     if (!success) {
       throw new Error("Mailgun failed to send email");
--- src/providers/SendGridProvider.ts
+++ src/providers/SendGridProvider.ts
@@ -1,11 +1,11 @@
 // A sample email provider that returns success / failure
-// Primary Provider
+// Fallback Provider
 
 import { EmailRequest } from "../models/EmailRequest";
 
 export class SendGridProvider {
   async sendEmail(email: EmailRequest): Promise<void> {
-    const success = Math.random() > 0.8; // computing a sample success chance. 40%
+    const success = Math.random() > 0.6; // computing a sample success chance. 40%
 
     if (!success) {
       throw new Error("SendGrid failed to send email");
